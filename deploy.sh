6b6be5c90586a7b39090c323aa023f8cb25741d1
Guillermo Ballesteros <guillermob@localhost.localdomain>
2020-12-07 17:21:33 -0600

    PrimerPost

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,6 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 
-printf "\033[0;32mDeploying updates to General GitHub...\033[0m\n"
-
-# Add changes
-git add .
-
-# Commit changes.
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin main
-
 printf "\033[0;32mDeploying updates to babiloneos.github.io...\033[0m\n"
 
 # Build the project.
